05d827809c44af748502acffc6dfe9d390bd0564
bjornsh <bjorn.schulte-herbruggen@ul.se>
2021-07-01 12:54:01 +0200

    script revamp

--- ftp_download.R
+++ ftp_download.R
@@ -1,65 +1,82 @@
-library(RCurl)
-library(readr)
-library(dplyr)
-library(writexl)
+##################################################################################
+### Script description
+##################################################################################
 
+# Download Kollektivtrafikbarometer .csv file from password protected FTP server 
+# and storing data as .xlsx file while avoiding encoding issues
+
+
+
+
+##################################################################################
+### Set up
+##################################################################################
+
+### clean start
 rm(list = ls())
+gc()
+
+
+### load libraries etc
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, RCurl, writexl)
+
+
+# avoid scientific notation
+options(scipen=999)
+
+
+### Create directories (if not already exist)
+dir.create(file.path(getwd(), "data"))
+dir.create(file.path(paste0(getwd(), "/data"), "input"))
+
+
+### Read keys, define paths 
+api_fil = read_file("Z:/api")
+server = gsub('^.*kollbar_server: \\s*|\\s*\r.*$', "", api_fil)
+userpwd = gsub('^.*kollbar_userpwd: \\s*|\\s*\r.*$', "", api_fil)
+
+folder_input = paste0(getwd(), "/data/input/")
 
 
 
-setwd("..")
+##################################################################################
+### Download .csv file from server
+##################################################################################
 
-api <- "Z:/" # location of file with keys
-output_csv <- paste0(getwd(),"/data_ul/csv/") # path to folder for csv file
-output_xlsx <- paste0(getwd(),"/data_ul/xlsx/")# path to folder for xlsx file
-
-
-##### DOWNLOAD CSV FILE FROM SFTP SERVER
-protocol <- "sftp"
-rkm <- "UL"
+### Define variables
+datum = "202102" # Download data for which month? (Format "YYYYMM")
+rkm = "UL" # Which RKM?
+protocol = "sftp"
 
 
-## Read FTP server keys
-api_fil <- read_file(paste0(api, "api"))
-server <- gsub('^.*kollbar_server: \\s*|\\s*\r.*$', "", api_fil)
-userpwd <- gsub('^.*kollbar_userpwd: \\s*|\\s*\r.*$', "", api_fil)
+### Name of file to be downloaded
+file_download = paste0("Databas Kollektivtrafikbarometern ", datum, " ", rkm)
 
 
+### get names of files on server
+url0 = paste0(protocol, "://", server) # for file names only
+filenames = getURL(url = url0, userpwd=userpwd, dirlistonly = TRUE)
+destnames = filenames <-  strsplit(filenames, "\r*\n")[[1]]
 
-## get filenames
-url0 <- paste0(protocol, "://", server) # for file names only
-filenames <- getURL(url = url0, userpwd=userpwd, dirlistonly = TRUE)
-destnames <- filenames <-  strsplit(filenames, "\r*\n")[[1]]
 
-
-## Download Data
-
-## create filename for last month
-tsfrFilename <- paste0("Databas Kollektivtrafikbarometern ",
-                  paste0(format(Sys.Date(), "%Y"), paste0("0", as.integer(format(Sys.Date(), "%m")) - 1)), " ",
-                  rkm)
-
-ouptFilename <- paste0(output_csv, tsfrFilename, ".csv")
-
-url <- paste0(protocol, "://", server, "/", tsfrFilename)
+### Download Data
+url <- paste0(protocol, "://", server, "/", paste0(file_download, ".csv"))
 data <- getURL(url = url, userpwd=userpwd)
 
-## Create File and save as csv
-fconn <- file(ouptFilename)
-write(data, fconn)
-close(fconn)
+
+# create local df
+# read.csv, read_delim etc failed to handle encoding
+df = data.table::fread(data, 
+                  sep = ";", 
+                  encoding = 'UTF-8')
+
+
+# Check if encoding is correct
+names(df)[1] # correct first column name?
+table(df$u_kommun) # correct Swedish spelling?
+
 
 ### Write as Excel
-dat = read.csv2(paste0(output_csv, tsfrFilename, ".csv"), encoding="UTF-8")
-
-dat = rename(dat, "A1" = 1) # csv import changes name of first column. Change back to correct name
-
-write_xlsx(dat, paste0(output_xlsx, tsfrFilename, ".xlsx"))
-
-
-
-
-
-
-
+write_xlsx(df, paste0(folder_input, "kollbar_", datum, ".xlsx"))
 
